081deb435842854c44f6570ef3d2a9c120d589ae
Samuel Fernandez <safernan@mbp-de-samuel.mas.lougeneste.com>
2021-04-24 18:45:47 +0200

    modif write

--- ft_write.s
+++ ft_write.s
@@ -3,14 +3,14 @@ section .text:
         extern __errno_location
 
 ft_write:
-    mov rax, 1 ; prepare rax pour le syscall write = 1
-    syscall ; call write
-    cmp rax, 0 ; compare retour de write a 0
+    mov rax, 1 ; syscall write = (rax = 1 )
+    syscall ; appel de write
+    cmp rax, 0 ; compare retour de write (rax) a 0
     jl error ; si rax < 0 jump a erreur
-    ret ; sinon return rax retourne par write
+    ret ; return rax (valeur retour de write)
 
 error:
-    mov r15, rax ; sauvegarde le retour de write = errno
+    mov r15, rax ; save le retour de write (errno)
     neg r15 ; inverse le signe
     call __errno_location ; donne l'addresse de errno
     mov [rax], r15 ; modifie errno
