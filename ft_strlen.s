e063a2d531ad0dc2881bc931c90d7458e089f5df
Samuel Fernandez <safernan@mbp-de-samuel.mas.lougeneste.com>
2021-04-24 19:00:42 +0200

    modif strlen

--- ft_strlen.s
+++ ft_strlen.s
@@ -2,16 +2,16 @@ section .text
 	global ft_strlen
  
 ft_strlen:
-	mov rax, 0 ; init compteur a 0
+	mov rax, 0 ; compteur a 0
 
-loop:
+boucle:
 	cmp byte [rdi + rax], 0 ; compare le byte de rdi (1ere parametre) a la position rax (compteur) au \0
 	je stop ; si flag equal on stop (\0 atteint)
-	jne continue ; si flag non equal on continue
+	jne ajoute ; si flag non equal on continue
 
-continue:
+ajoute:
 	inc rax ; incremente rax
-	jmp loop ; retourne a la fonction loop
+	jmp boucle ; retourne a la fonction boucle
 
 stop:
 	ret ; return final
